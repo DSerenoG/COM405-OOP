eb34fbbb7526202dc325485ccb85e343c8614877
Daniel Sereno Guedes <4sered52@solent.ac.uk>
2018-10-23 17:47:47 +0100

    Week5

---
+++ Week5/src/week5/Bus.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week5;
+
+/**
+ *
+ * @author 4SERED52
+ */
+public class Bus 
+{
+    private Passenger[] busNum;
+    private boolean addPassenger;
+    
+    
+    
+    
+    public Bus()
+    {
+        busNum = new Passenger[15];
+    }
+    
+    public boolean addPassenger(Bus busIn)
+    {
+        for(int i=0; i < 15; i++)
+        {
+            if()
+            {
+              
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Week5/src/week5/Passenger.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package week5;
+
+/**
+ *
+ * @author 4SERED52
+ */
+public class Passenger 
+{
+ private String passenger;
+ private int age;
+ 
+ public Passenger(String passNumIn )
+ {
+     passenger = passNumIn;
+ }
+ 
+ public String getpassenger()
+ {
+     return passenger;
+ }
+ 
+ public int getage()
+ {
+     return age;
+ }
+}
